05b0f58da00023bf3f2b556e9f44532fe36b2a34
vansthescorpion <vanessagasparphotos@gmail.com>
2025-05-20 18:45:34 +0100

    Removed Unity VCS / Plastic SCM config

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -5,33 +5,35 @@ public class ItemPickup : MonoBehaviour
     public ItemData itemData; // Referência ao ScriptableObject do item
     public int amount = 1;
 
+
+    // Para items que vão para o Gabriel mas tens de clicar B
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Gabriel") && itemData.type == ItemData.ItemType.Gabriel)
         {
             if (Input.GetKeyDown(KeyCode.B))
             {
-                GabrielInventory inventory = other.GetComponent<GabrielInventory>();
+                /*GabrielInventory inventory = other.GetComponent<GabrielInventory>();
                 if (inventory != null)
                 {
                     inventory.AddItem(itemData, amount);
-                    Destroy(gameObject); // Remove o objeto da cena
-                }
+                    Destroy(gameObject); // tira o objeto da cena
+                }*/
             }
         }
     }
 
-    // Para itens que vão automaticamente para a Peralta
+    // Para items que vão automaticamente para a Peralta
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Peralta") && itemData.type == ItemData.ItemType.Peralta)
         {
-            PeraltaInventory inventory = other.GetComponent<PeraltaInventory>();
+            /*PeraltaInventory inventory = other.GetComponent<PeraltaInventory>();
             if (inventory != null)
             {
                 inventory.AddItem(itemData, amount);
                 Destroy(gameObject);
-            }
+            }*/
         }
     }
     
