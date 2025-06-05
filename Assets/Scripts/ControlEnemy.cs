d3a72e93442699e4f36bb82c8ef493f4bd2afb16
TiagoAndrade2005 <tiago.l.t.andrade@gmail.com>
2025-06-05 12:52:17 +0100

    Enemy 2.0

--- Assets/Scripts/ControlEnemy.cs
+++ Assets/Scripts/ControlEnemy.cs
@@ -1,3 +1,7 @@
+using System.Collections;
+using System.Linq.Expressions;
+using UnityEditor.Rendering.Analytics;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using static UnityEditor.PlayerSettings;
@@ -7,12 +11,19 @@ public class ControlEnemy : MonoBehaviour
     public float Velocidade;
     public bool direcao;
     public string Estate;
+    public GameObject AlvoGOB;
+    public float Life;
+    public float Radius;
+    public float CoolDown;
 
     void Start()
     {
             Velocidade = 5f;
             Estate = "Idle";
+            
+            
     }
+    
 
     // Update is called once per frame
     void Update()
@@ -20,34 +31,85 @@ public class ControlEnemy : MonoBehaviour
         Vector3 Pos = this.transform.position;
         Pos.x = Pos.x + Velocidade * Time.deltaTime;
 
+
+        // decidir estado
+        Vector3 Df = AlvoGOB.transform.position - this.transform.position;
+        
+        if (Df.magnitude < Radius && Estate=="Idle")
+        {
+        
+            Estate = "Active";
+        }
+
+
+
+        if (Life == 0)
+        {
+            gameObject.GetComponent<Animator>().Play("Death_Inimigo01");
+        }
+        
+        //sistema de movimento base
+
         if (Estate == "Idle")
         {
             if (Pos.x >= 10)
             {
                 this.transform.eulerAngles = new Vector3(0, 0, 180);
+                direcao = true;
                 Velocidade = Velocidade * -1;
 
             }
-          /*  if (Pos.x <= -10)
+            if (direcao == true && Pos.x <= -10)
             {
-                this.transform.eulerAngles = new Vector3(0, 0, 180);
+                print("Vira");
+                this.transform.eulerAngles = new Vector3(0, 0, 0);
+                direcao = false;
                 Velocidade = Velocidade * -1;
-            }*/
-                this.transform.position = Pos;
-        }
-        else
-        {
-            /*if (Pos.x <= -10)
-            {
-                Pos.x = 10;
+
             }
+
             this.transform.position = Pos;
         }
-        if (Keyboard.current.rightArrowKey.wasPressedThisFrame)
+        //sistema de ataque perto
+       
+        if (Estate == "Active")
         {
-            this.transform.eulerAngles = new Vector3(0, 0, -90);
-            this.transform.Translate(Incremento, 0, 0, Space.World);
-            */
+            gameObject.GetComponent<Animator>().Play("Active_Inimigo01");
+            Velocidade = 5f;
+            Vector3 Dif = AlvoGOB.transform.position - this.transform.position;
+            Dif.Normalize();
+            Dif = Time.deltaTime * Velocidade * Dif;
+            this.transform.Translate(Dif, Space.World);
+
+        }
+        //sistema de rest
+        if (Estate == "Tired")
+        {
+            gameObject.GetComponent<Animator>().Play("Tired_Inimigo01");
+            Invoke("passaidle", CoolDown);
+            ;
+        }
+
+
+    }
+    
+    void passaidle()
+    {
+        Estate = "Idle";
+    }
+    
+    //Sistema de dano
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+      //  GabrielHealthBar = GabrielHealthBar - 1;
+
+        if (Estate == "Active")
+        {
+            Estate = "Tired";
         }
     }
+   
+   
 }
+
+
