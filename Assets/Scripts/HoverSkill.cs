40d9338571fb3b6f1738fa8fc33baa636fde7579
TiagoAndrade2005 <tiago.l.t.andrade@gmail.com>
2025-06-07 18:40:31 +0100

    Peralta_Float01

--- Assets/Scripts/HoverSkill.cs
+++ Assets/Scripts/HoverSkill.cs
@@ -1,10 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HoverSkill : MonoBehaviour
 {
+    public float Time;
+    private Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+       
+    }
+
+
     public void Execute()
     {
         Debug.Log("Peralta executou HoverSkill");
+        print("Floot 0");
+        if (Time > 0)
+        {
+            if (animator != null)
+            {
+                animator.Play("Hover_Peralta01");
+                
+
+            }
+            this.gameObject.layer = LayerMask.NameToLayer("Floot");
+            print("Floot 1");
+            Invoke("terminafloot", Time);
+            
+        }
         
+    }
+
+    void terminafloot()
+    {
+        animator.Play("Idle_Peralta01");
+        print("Floot fim");
+        this.gameObject.layer = LayerMask.NameToLayer("Peralta");
+
     }
 }
