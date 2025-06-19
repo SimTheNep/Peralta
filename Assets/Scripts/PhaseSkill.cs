6c2e054997fa8acc7d276e7844789397baf2cd0f
TiagoAndrade2005 <tiago.l.t.andrade@gmail.com>
2025-06-19 12:28:55 +0100

    Fasemento_Peralta_02

--- Assets/Scripts/PhaseSkill.cs
+++ Assets/Scripts/PhaseSkill.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 public class PhaseSkill : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+
+    }
     public void Execute()
     {
-        Debug.Log("Peralta executou PhaseSkill");
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Grade"))
+        {
 
+        }
     }
 }
