bf187ccec25cf211ccf79189269454aec3311715
TiagoAndrade2005 <tiago.l.t.andrade@gmail.com>
2025-06-21 17:18:25 +0100

    Block_Water + Float 2

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,8 +1,28 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public enum BoxType { Light, Heavy }
 
 public class Box : MonoBehaviour
 {
     public BoxType boxType = BoxType.Light;
+    private Tilemap Water;
+
+    private void Start()
+    {
+        Water = GetComponent<Grid/>();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Water_Entry"))
+        {
+            print("Tapou");
+            Tilemap tilemap = GetComponent<Tilemap> ();
+            tilemap.SetTile(new Vector3Int(0, 0, 0), null);
+        }
+    }
 }
+
+
